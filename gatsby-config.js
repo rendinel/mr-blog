21649b9174510e70cd576c1f8a68f5dfbe45a468
rendinel <rendinel@protonmail.com>
2022-05-07 10:39:10 +0400

    added source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,5 +17,26 @@ module.exports = {
         isUsingColorMode: true,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
   ],
 }
